e9cf94f11f78c3c02ce9310a9ff3eb0ae65bcfd2
KEBE Mancha <mancha.kebe@gmail.com>
2023-11-16 10:38:41 +0100

    version 0.1 Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import styles from '@/styles/Header.module.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faUser} from '@fortawesome/free-regular-svg-icons';
+import {faCloud} from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   return (
     <div className ={styles.container}>
       <div className={styles.left}>
       <p>Date</p>
-      <p>temperature</p>
-      <p>weather icon</p>
+      <div className={styles.block}>
+        <p><FontAwesomeIcon icon={faCloud} />temperature</p>
+        
       </div>
-      <div>
-        <h1>Title</h1> 
       </div>
+      <h1 className={styles.title}>Zhe Journal</h1> 
       <div className={styles.right}>
         <p>welcome...</p>
-        <span>connexion</span>
+        <div className={styles.block}>
+          <FontAwesomeIcon icon={faUser} />
+          <span><br/>connexion</span>
+        </div>
+        <i class="fa fa-user-o" aria-hidden="true"></i>
         </div>
     </div>
   )
