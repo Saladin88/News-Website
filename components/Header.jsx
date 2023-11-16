825e62827ce747197aca6f59419d93e1b1e791f1
Simon Duflot <narcisse.duflot@gmail.com>
2023-11-16 10:38:37 +0100

    version 0.1 article

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,6 @@
-import React from 'react';
-import styles from '@/styles/Header.module.css';
-
+import React from "react";
+import styles from "@/styles/Header.module.css";
 
 export default function Header() {
-  return (
-    <div className ={styles.container}>
-      <h1 className = {styles.title}>Title</h1>
-      <div className = {styles.items}>Date</div>
-      <div className = {styles.items}>temperature</div>
-      <div className = {styles.items}>weather icon</div>
-      <div className = {styles.items}>welcome...</div>
-      <div className = {styles.items}>connexion</div>
-    </div>
-  )
+  return;
 }
